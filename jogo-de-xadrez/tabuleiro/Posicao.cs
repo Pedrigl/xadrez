44e69174366261476f62307748922055eef827d2
Pedrigl <pedrogabrielcruzlima@gmail.com>
2022-05-21 16:29:10 -0300

    Adicionar arquivos de projeto.

---
+++ jogo-de-xadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        public int coluna { get; set; }
+
+        public int linha { get; set; }
+
+        public Posicao( int coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return coluna + ", " + linha;
+        }
+
+    }
+}
